028c1773033458f4a419ebcfdf56ebf0e482329e
Hoang Nguyen <hoangnguyencs96@gmail.com>
2017-09-30 23:41:59 +0700

    Submit lan 1

--- QLKSver2.0/QLKSver2.0/QL_SDDV.cs
+++ QLKSver2.0/QLKSver2.0/QL_SDDV.cs
@@ -19,6 +19,40 @@ namespace QLKS
             InitializeComponent();
         }
         QLTPDataContext db;
+        public void Show_TreeView()
+        {
+            tv_khach.Nodes.Clear();
+            var query_cha = (from kh in db.Khaches
+                             select kh);
+            List<Khach> khg = new List<Khach>();
+            khg = query_cha.ToList<Khach>();
+            for (int i = 0; i < khg.Count - 1; i++)
+            {
+                tv_khach.Nodes.Add(khg[i].CMT, khg[i].Hoten);
+                string cmt = khg[i].CMT;
+                var query_con = (from tp in db.ThuePs
+                                 where tp.CMT == cmt && tp.Ngaydi == null
+                                 select tp);
+                List<ThueP> tpg = new List<ThueP>();
+                tpg = query_con.ToList<ThueP>();
+                for (int j = 0; j < tpg.Count - 1; j++)
+                {
+                    var query = (from ph in db.Phongs
+                                     //group ph by ph.MaP into kq
+                                 where ph.MaP == tpg[j].MaP
+                                 select ph);
+                    List<Phong> phong = new List<Phong>();
+                    phong = query.ToList<Phong>();
+                    tv_khach.Nodes[i].Nodes.Add(phong[0].MaP, phong[0].TenP);
+                }
+            }
+        }
+        public void Show_DV()
+        {
+            var query = (from n in db.Dichvus
+                         select n);
+            dichvuBindingSource.DataSource = query.ToList<Dichvu>();
+        }
 
         private void QL_SDDV_Load(object sender, EventArgs e)
         {
