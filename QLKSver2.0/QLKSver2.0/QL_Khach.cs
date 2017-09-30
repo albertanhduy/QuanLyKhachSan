7ff8b22f7f9a9185360f07cae9193bf70a19c3b2
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2017-09-30 09:02:53 +0700

    First

--- QLKSver2.0/QLKSver2.0/QL_Khach.cs
+++ QLKSver2.0/QLKSver2.0/QL_Khach.cs
@@ -19,6 +19,30 @@ namespace QLKS
             InitializeComponent();
         }
         QLTPDataContext db = new QLTPDataContext();
+        public void anhien(Boolean gt)
+        {
+            txt_cmt.Enabled = gt;
+            txt_dchi.Enabled = gt;
+            txt_dt.Enabled = gt;
+            cbb_gtinh.Enabled = gt;
+            txt_ten.Enabled = gt;
+        }
+
+        public void khoacn(Boolean gt)
+        {
+            btn_sua.Enabled = gt;
+            btn_them.Enabled = gt;
+            btn_xoa.Enabled = gt;
+        }
+        private void Display()
+        {
+            List<Khach> list = new List<Khach>();
+            var query = (from n in db.Khaches
+                         select n);
+            list = query.ToList<Khach>();
+            khachBindingSource.DataSource = list;
+            //            khachDataGridView.AutoGenerateColumns = false;
+        }
         private void QL_Khach_Load(object sender, EventArgs e)
         {
             
