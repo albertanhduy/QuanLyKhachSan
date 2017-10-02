31c5670de6d26c8baf1d493d44671eb7c90ac3ce
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2017-10-02 15:07:36 +0700

    Third

--- QLKSver2.0/QLKSver2.0/QL_Khach.cs
+++ QLKSver2.0/QLKSver2.0/QL_Khach.cs
@@ -62,15 +62,93 @@ namespace QLKS
         }
         public bool ktra_sdt(string sdt)
         {
-            
+            int sdthoai;
+            Regex reg = new Regex("\\d{10,11}");
+            if (reg.IsMatch(sdt) && (int.TryParse(sdt, out sdthoai) == true))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         public void Set_Empty()
         {
-            
+            txt_cmt.Text = "";
+            txt_dchi.Text = "";
+            txt_dt.Text = "";
+            txt_ten.Text = "";
+            cbb_gtinh.Text = "";
         }
         private void btn_them_Click(object sender, EventArgs e)
         {
-
+            if (btn_them.Text == "Thêm")
+            {
+                btn_them.Text = "Lưu lại";
+                anhien(true);
+                khachBindingSource.AddNew();
+                khachdataGridView1.BeginEdit(true);
+                khoacn(false);
+                txt_cmt.Focus();
+                btn_them.Enabled = true;
+            }
+            else
+            {
+                if ((txt_cmt.Text != "") && (txt_ten.Text != "") && (txt_dchi.Text != "") && (cbb_gtinh.Text != "") && (txt_dt.Text != ""))
+                {
+                    if (ktra_cmt(txt_cmt.Text) == true)
+                    {
+                        if (ktra_sdt(txt_dt.Text) == true)
+                        {
+                            Khach ktra = (from khach in db.Khaches
+                                          where khach.CMT == txt_cmt.Text.Trim()
+                                          select khach).SingleOrDefault();
+                            if (ktra == null)
+                            {
+                                khachBindingSource.EndEdit();
+                                Khach khachs = new Khach();
+                                khachs.CMT = txt_cmt.Text;
+                                khachs.Hoten = txt_ten.Text;
+                                khachs.Diachi = txt_dchi.Text;
+                                khachs.GT = cbb_gtinh.Text;
+                                khachs.SDT = txt_dt.Text;
+                                db.Khaches.InsertOnSubmit(khachs);
+                                db.SubmitChanges();
+                                anhien(false);
+                                MessageBox.Show("Thành công!", "Thêm khách hàng");
+                                btn_them.Text = "Thêm";
+                                khoacn(true);
+                                khachDataGridView.Refresh();
+                                Display();
+                            }
+                            else
+                            {
+                                khachBindingSource.CancelEdit();
+                                Set_Empty();
+                                MessageBox.Show("Số CMND này đã được đăng ký. Vui lòng kiểm tra lại");
+                                txt_cmt.Focus();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nhập vào số điện thoại không đúng", "Lỗi");
+                            txt_dt.Text = "";
+                            txt_dt.Focus();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bạn phải nhập chính xác số chứng minh nhân dân", "Lỗi");
+                        txt_cmt.Text = "";
+                        txt_cmt.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Bạn phải nhập vào đầy đủ dữ liệu", "Lỗi");
+                }
+            }
         }
 
         private void btn_sua_Click(object sender, EventArgs e)
