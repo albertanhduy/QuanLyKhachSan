d0349bf0f7b0b6ba1bfbdd073c8e1ed105cbf7d8
phamhungdg96 <phamhungdg96@gmail.com>
2017-10-02 07:29:24 +0700

    update masterdetail

--- QLKSver2.0/QLKSver2.0/QL_Chothuephong.cs
+++ QLKSver2.0/QLKSver2.0/QL_Chothuephong.cs
@@ -49,17 +49,39 @@ namespace QLKS
 
         private void cbb_loaip_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+           List<Phong> list = new List<Phong>();
+            var query2 = (from phong in db.Phongs
+                          where phong.LoaiP == cbb_loaip.Text && phong.Tinhtrang == "Trống"
+                          select phong);
+            list = query2.ToList<Phong>();
+            phongBindingSource.DataSource = list;
         }
 
         private void txt_cmt_TextChanged(object sender, EventArgs e)
         {
-           
+            var query = (from n in db.Khaches
+                         where n.CMT.Contains(txt_cmt.Text)
+                         select n);
+            khachBindingSource.DataSource = query.ToList<Khach>();
         }
 
         private void btn_themk_Click(object sender, EventArgs e)
         {
-            
+            if (btn_themk.Text == "Thêm khách")
+            {
+                btn_chonk.Enabled = false;
+
+                btn_themk.Text = "Cập nhật bảng";
+                QL_Khach khach = new QL_Khach();
+                khach.ShowDialog();
+            }
+            else
+            {
+                khachBindingSource.DataSource = null;
+                ShowKhach();
+                btn_themk.Text = "Thêm khách";
+                btn_chonk.Enabled = true;
+            }
         }
 
         private void btn_chonp_Click(object sender, EventArgs e)
